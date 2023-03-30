7bfdcfbcda0aff9e04f3c9933bc5987ab50bf757
WIRYEFARMER <dusrb1418@naver.com>
2023-03-30 16:50:12 +0900

    자바수업 17일차

---
+++ day17/lotto/Ball.java
@@ -0,0 +1,16 @@
+package kr.ac.kopo.day17.lotto;
+
+public class Ball {
+	private int number;
+	
+	public Ball (int number) {
+		this.number=number;
+	}
+	
+	public int getNumber() {
+		
+		
+		return this.number;
+	}
+
+}
---
+++ day17/lotto/Machine.java
@@ -0,0 +1,28 @@
+package kr.ac.kopo.day17.lotto;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Machine {
+	
+	private List<Ball> balls;
+	
+	public Machine() {
+		balls = new ArrayList<>();
+		
+		for(int i=1; i<=45; i++) {
+			Ball ball = new Ball(i);
+			balls.add(ball);
+		}
+	}
+	public void start() {
+		Collections.shuffle(balls);
+	}
+	
+	public Ball getBall() {
+		return balls.remove(0);
+		
+	}
+
+}
